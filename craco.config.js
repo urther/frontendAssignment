b912ddbae47163a528fee0909f93652d1790d386
urther <alswlkku@ajou.ac.kr>
2022-06-30 00:10:51 +0900

    chore : Add a path alias (hooks directory)
    - Add hooks path alias

--- craco.config.js
+++ craco.config.js
@@ -7,6 +7,7 @@ module.exports = {
       "@styles": path.resolve(__dirname, "src/styles/"),
       "@assets": path.resolve(__dirname, "src/assets/"),
       "@components": path.resolve(__dirname, "src/components/"),
+      "@hooks": path.resolve(__dirname, "src/hooks/"),
     },
   },
   jest: {
---
+++ src/hooks/useCounter.ts
@@ -0,0 +1,3 @@
+const useCounter = () => {};
+
+export default useCounter;
