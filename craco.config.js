51279f781c6d40e1bcb77457c7e7d2a31b82f1d3
urther <alswlkku@ajou.ac.kr>
2022-06-29 23:36:20 +0900

    chore : Add path alias(@components, @assets)
    - Add path alias @components
    - Add path alias @assets

--- craco.config.js
+++ craco.config.js
@@ -5,6 +5,8 @@ module.exports = {
   webpack: {
     alias: {
       "@styles": path.resolve(__dirname, "src/styles/"),
+      "@assets": path.resolve(__dirname, "src/assets/"),
+      "@components": path.resolve(__dirname, "src/components/"),
     },
   },
   jest: {
